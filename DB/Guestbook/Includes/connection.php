c6b9325cd024692f6de1d08f2d9fc94ce7af2475
KT <kt@easv.dk>
2024-09-11 20:49:35 +0200

    DB files for lecture 3

--- DB/Guestbook/Includes/connection.php
+++ DB/Guestbook/Includes/connection.php
@@ -1,6 +1,1 @@
 <?php
-require("constants.php");
-try{$db = new PDO(DSN, DB_USER, DB_PASS);}
-catch (PDOException $err) {
-    echo 'Connection failed: ' . $err->getMessage();
-}
--- DB/Guestbook/Includes/constants.php
+++ DB/Guestbook/Includes/constants.php
@@ -1,8 +1,3 @@
 <?php
-$db_dbname = "mysql:dbname=guest";
-$db_host = "host=localhost";
-$db_charset = "charset=utf8";
-define("DSN", "$db_dbname; $db_host; $db_charset");
-define("DB_USER", "root");
-define("DB_PASS", "123456");
+
 
